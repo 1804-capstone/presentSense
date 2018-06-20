e234f3e5315b86747474bd9b88f21465c419d9cb
Monica Gonzalez <mogon2990@gmail.com>
2018-06-20 16:12:24 -0400

    component file structure

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation'
+import { Login, Home, Dashboard } from './components'
+
+const StackNavigator = createStackNavigator({
+  LoginScreen: {screen: Login},
+  HomeScreen: {screen: Home},
+  Dashboard: {screen: Dashboard}
+  }, {
+    initialRouteName: 'LoginScreen'
+  })
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <StackNavigator />
+      // <View style={styles.container}>
+      //   <Text>Hello</Text>
+      //   <Text>Hey</Text>
+      //   <Text>Shake your money maker</Text>
+      // </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+//this file is the 'central export' for our components
+
+export { default as Login } from './Login'
+export { default as Home } from './Home'
+export { default as Dashboard } from './Dashboard'
