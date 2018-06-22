70cec1fc40cc3ec8e392bcfa0567c28af845ccac
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-06-22 12:19:07 -0400

    working on our reducer for heart rate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "react-navigation";
 import { Provider } from "react-redux";
+//redux store
+import store from "./store";
 import {
   Login,
   Home,
