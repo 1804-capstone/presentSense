95c06d5b2cc21fccdd7ec8fc0c7bd7b088d23464
Monica Gonzalez <mogon2990@gmail.com>
2018-06-23 18:06:54 -0400

    draggable drawer

--- src/components/Home.js
+++ src/components/Home.js
@@ -69,7 +69,7 @@ export default class Home extends React.Component {
           large={true}
           fontSize={40}
           backgroundColor="#26A69A"
-          onPress={() => navigate("MyAccount")}
+          onPress={() => navigate("Dashboard")}
         />
         <Button
           title="My Entries"
