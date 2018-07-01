85504baa2f4ada33e3a1ecf5a5d1f024216338d8
Arielle Gordon <reallygordon@gmail.com>
2018-07-01 15:20:02 -0400

    added sleep sample reducer with sleep sample thunk, which is dispatched on GraphMaker CDM

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+// :)
 //import individual reducers for data sources
 import heartRate from "./heartrate";
 import steps from "./steps";
-import firestoreStore from './firebase'
+import firestoreStore from "./firebase";
+import sleep from "./sleep";
 
 const reducer = combineReducers({
   heartRate,
   steps,
-  firestoreStore
+  firestoreStore,
+  sleep
 });
 
 const middleware = composeWithDevTools(
